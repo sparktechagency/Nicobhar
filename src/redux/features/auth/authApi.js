841c30ec87ec380a0e02d29a416173d4acbe0585
Ksh Siaan <kshsiaan@gmail.com>
2025-05-21 13:38:51 +0600

    sup adm

--- src/redux/features/auth/authApi.js
+++ src/redux/features/auth/authApi.js
@@ -4,8 +4,8 @@ export const authApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
     login: builder.mutation({
       query: (userInfo) => ({
-        url: '/auth/login',
-        method: 'POST',
+        url: "/auth/login",
+        method: "POST",
         body: userInfo,
       }),
       invalidatesTags: ["auth"],
@@ -13,8 +13,8 @@ export const authApi = baseApi.injectEndpoints({
 
     forgetPassword: builder.mutation({
       query: (userEmail) => ({
-        url: '/auth/forgot-password',
-        method: 'POST',
+        url: "/auth/forgot-password",
+        method: "POST",
         body: userEmail,
       }),
       invalidatesTags: ["auth"],
@@ -22,17 +22,17 @@ export const authApi = baseApi.injectEndpoints({
 
     otpVerify: builder.mutation({
       query: (otp) => ({
-        url: '/auth/verify',
-        method: 'POST',
+        url: "/auth/verify",
+        method: "POST",
         body: otp,
       }),
       invalidatesTags: ["auth"],
     }),
-    
+
     createNewPassword: builder.mutation({
       query: (newPasswordInfo) => ({
-        url: '/auth/reset-password',
-        method: 'POST',
+        url: "/auth/reset-password",
+        method: "POST",
         body: newPasswordInfo,
       }),
       invalidatesTags: ["auth"],
@@ -40,4 +40,9 @@ export const authApi = baseApi.injectEndpoints({
   }),
 });
 
-export const { useLoginMutation, useForgetPasswordMutation,useOtpVerifyMutation,useCreateNewPasswordMutation } = authApi;
+export const {
+  useLoginMutation,
+  useForgetPasswordMutation,
+  useOtpVerifyMutation,
+  useCreateNewPasswordMutation,
+} = authApi;
